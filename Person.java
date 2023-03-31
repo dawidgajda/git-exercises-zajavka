4015f0d241dde73eb015f24c06e05ae6262902ce
Dawid Gajda <dawid.gajda3@gmail.com>
2023-03-31 14:27:41 +0200

    feat: [TASK-124] Commit from Intellij

--- Person.java
+++ Person.java
@@ -1,10 +1,11 @@
 public class Person {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello!");
-		int a = 3;
-		String b = "abc";
-		int c = 2;
-	}
-	
+
+    public static void main(String[] args) {
+        // asdsadas
+        System.out.println("Hello!");
+        int a = 3;
+        int c = 2;
+        String abc = "asd";
+    }
+
 }
